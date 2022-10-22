f8a74cc9dbe74b05f39eb5d226575f9c5f235cbd
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2022-10-21 21:41:57 -0400

    master

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-let randomDamage = () => { return Math.floor(Math.random() * 10) + 1 };
+let randomDamage = () => { return Math.floor(Math.random() * 10) + 1 }
 
 let chooseOption = (opt1, opt2) => {
 
@@ -19,25 +19,28 @@ let logDeath = (winner, loser) => {
     console.log(`${winner} defeated ${loser}`)
 }
 
-let isDead = (health) => health <= 0 ? true : false;
+let isDead = (health) => health <= 0 ? true : false
 
 let fight = function (player1, player2, player1Health, player2Health){
     while (true) {
-        let attacker = chooseOption (player1, player2);
+        let attacker = chooseOption (player1, player2)
         if (attacker === player1) {
-            player2Health = attackPlayer(player2Health);
+            player2Health = attackPlayer(player2Health)
             logHealth(player2, player2Health)
-                 if (isDead(player2Health === true)){
-                    logDeath(player1, player2);}
-                    break;
+                 if (isDead(player2Health)){
+                    logDeath(player1, player2)
+                    break
                 }
-                    else {
-                    let player1Health = attackplayer(player1Health);
-                        logHealth(player1, player1Health);
-                        if (isDead(player1Health)){
-                            logDeath(player1, player2)
-                            break;
             }
+                    else {
+                     player1Health = attackPlayer(player1Health)
+                        logHealth(player1, player1Health)
+                        if (isDead(player1Health)){
+                            logDeath(player2, player1)
+                            break
+            }
+        
         }
     }
 }
+fight("Freddy", "Jason", 125, 100)
